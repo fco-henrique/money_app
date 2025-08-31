66b8299415e8daf3037592fb6833b7df91cac9ef
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-08-30 21:38:02 -0300

    feat: change the home screen

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:money_app/common/constants/routes.dart';
-import 'package:money_app/services/secure_storage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,26 +8,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _secureStorage = const SecureStorage();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Mael Feioso"),
-            ElevatedButton(
-              onPressed: () {
-                _secureStorage.deleteOne(key: "CURRENT_USER").then(
-                  (_) => Navigator.popAndPushNamed(context, NamedRoutes.initial)
-                );
-              }, 
-              child: Text("Logout")
-            )
-          ],
-        ),
+        child: Text("Home"),
       ),
     );
   }
