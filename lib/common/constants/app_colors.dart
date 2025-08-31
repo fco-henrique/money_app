76d3d3aa9f6b81d300f9a65eadd9d183a8a38453
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-08-30 21:34:52 -0300

    style: update app colors, new color lightGrey

--- lib/common/constants/app_colors.dart
+++ lib/common/constants/app_colors.dart
@@ -9,6 +9,7 @@ class AppColors {
   static const Color white = Color(0xFFFFFFFF);
   static const Color darkGrey = Color(0xFF444444);
   static const Color grey = Color(0xFF666666);
+  static const Color lightGrey = Color(0xFFAAAAAA);
   static const List<Color> greenGradient = [Color(0xFF63B5AF), Color(0xFF438883)];
   static const List<Color> greyGradient = [Color(0xFFC1C4C4), Color(0xFF767776)];
   static const List<Color> testGradient = [Color(0xFF6829AC), Color(0xFF2D2E66)];
