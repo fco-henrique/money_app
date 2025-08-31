8cdd5d338e25e9abc11808e115eeb97cefbe75a9
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-08-30 21:41:05 -0300

    fix/update: changing the home route and inserting the stats, profile and wallet routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:money_app/common/constants/routes.dart';
-import 'package:money_app/features/home/home_page.dart';
-// import 'package:money_app/common/themes/default_theme.dart';
+import 'package:money_app/features/home/home_page_view.dart';
 import 'package:money_app/features/onboarding/onboarding_page.dart';
+import 'package:money_app/features/profile/profile_page.dart';
 import 'package:money_app/features/sign_in/sign_in_page.dart';
 import 'package:money_app/features/sign_up/sign_up_page.dart';
 import 'package:money_app/features/splash/splash_page.dart';
+import 'package:money_app/features/stats/stats_page.dart';
+import 'package:money_app/features/wallet/wallet_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -20,7 +22,10 @@ class App extends StatelessWidget {
         NamedRoutes.splash: (context) => SplashPage(),
         NamedRoutes.signUp: (context) => SignUpPage(),
         NamedRoutes.signIn: (context) => SignInPage(),
-        NamedRoutes.home: (context) => HomePage(),
+        NamedRoutes.home: (context) => HomePageView(),
+        NamedRoutes.profile: (context) => ProfilePage(),
+        NamedRoutes.stats: (context) => StatsPage(),
+        NamedRoutes.wallet: (context) => WalletPage(),
       },
     );
   }
