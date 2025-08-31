e6b34b2d7f04b93ece56d8d5ecbe56e57946631f
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-08-30 21:39:03 -0300

    feat: creation of stats, profile and wallet screens and routes

--- lib/common/constants/routes.dart
+++ lib/common/constants/routes.dart
@@ -6,5 +6,8 @@ class NamedRoutes {
   static const String signUp = "/sign_up";
   static const String signIn = "/sign_in";
   static const String home = "/home";
+  static const String stats = "/stats";
+  static const String profile = "/profile";
+  static const String wallet = "/wallet";
 
 }
