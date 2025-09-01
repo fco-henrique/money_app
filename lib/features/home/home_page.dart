725cbfa91f8e3e05bdc7389b9eff5e3c3e12c03a
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-09-01 16:24:18 -0300

    feat: added the first container that will contain the information

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:money_app/common/constants/app_colors.dart';
+import 'package:money_app/common/constants/app_text_styles.dart';
+import 'package:money_app/common/extensions/sizes.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +13,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double get textScaleFactor => MediaQuery.of(context).size.width < 360? 0.7 : 1.0;
+  double get iconSize => MediaQuery.of(context).size.width < 360 ? 16.0 : 24.0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home"),
-      ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: AppColors.greenGradient,
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.elliptical(500, 30),
+                  bottomRight: Radius.elliptical(500, 30),
+                )
+              ),
+              height: 287.h,
+            )
+          ),
+        ],
+      )
     );
   }
 }
