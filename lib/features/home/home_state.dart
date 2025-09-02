d2c22a1d26a65011bc03137ee9dd0475e0340f2f
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-09-02 15:57:38 -0300

    chore: add home state

---
+++ lib/features/home/home_state.dart
@@ -0,0 +1,9 @@
+abstract class HomeState {}
+
+class HomeInitialState extends HomeState {}
+
+class HomeSuccessState extends HomeState {}
+
+class HomeLoadingState extends HomeState {}
+
+class HomeErrorState extends HomeState {}
