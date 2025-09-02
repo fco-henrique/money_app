157acb79ccf846aff028e066b15885e9619b2037
Henrique Nunes <henriquesilvanunes0015@gmail.com>
2025-09-02 15:56:16 -0300

    style: added color property to CustomCircularProgressIndicator

--- lib/common/widgets/custom_circular_progress_indicator.dart
+++ lib/common/widgets/custom_circular_progress_indicator.dart
@@ -4,14 +4,17 @@ import 'package:flutter/material.dart';
 import '../constants/app_colors.dart';
 
 class CustomCircularProgressIndicator extends StatelessWidget {
+  final Color? color;
+
   const CustomCircularProgressIndicator({
     super.key,
+    this.color,
   });
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CircularProgressIndicator(color: AppColors.white,),
+      child: CircularProgressIndicator(color: color?? AppColors.white,),
     );
   }
 }
